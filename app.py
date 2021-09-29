62b990b8ed111c9410873b02487cac345530d3a4
Wessel Steenkamp <66951387+wesse206@users.noreply.github.com>
2021-09-29 15:44:45 +0200

    Update. TODO: Remove Homepage navbar bug

--- app.py
+++ app.py
@@ -2,8 +2,25 @@ from flask import *
 app = Flask(__name__)
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/studentspace')
+@app.route('/subject-choices')
+def subjectchoices():
+    return 'Subject Choices'
+
+@app.route('/admission-policy')
+def admissionpolicy():
+    return 'Admission Policy'
+
+@app.route('/shool-fees')
+def schoolfees():
+    return 'School Fees'
+
+@app.route('/circulars')
+def circulars():
+    return 'Circulars'
+
+@app.route('/learners')
 def studentspace():
-    return render_template('studentspace.html')
+        return render_template('learners.html')
+
